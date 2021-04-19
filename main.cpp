d19bc47d86e89dac579e6ce1526e7593056b898a
yasinkuyuk <yasin.kuyuk@albert.health>
2021-04-19 21:09:03 +0300

    map structure DOES NOT WORK. last commit with map

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include<list>
 #include<vector>
 #include <algorithm>
+#include<map>
 
 
 using namespace std;
@@ -29,24 +30,42 @@ class Edge{
         }
 };
 
+
 class CityGraph{
     private:
-        int currentEdgeCount;
+        int currentVerticeCount;
         vector<Edge> edges;
+        vector<string> vertices;
+        map<string,int> parents;
+        vector<Edge> minimumSpanningTree;
 
     public:
         void sortEdges();
         void addEdge(Edge);
         void print();
+        int getVerticeCount(){return this->currentVerticeCount;}
+
+        void kruskalAlgorithm();
+        int findParent(map<string,int>,int);
+        void changeParents(map<string,int>, int, int);
+        void createParents();
 
         CityGraph(){
-            this->currentEdgeCount = 0;
+            this->currentVerticeCount = 0;
         }
 };
 
 void CityGraph::addEdge(Edge edge){
+    if(!(find(this->vertices.begin(), this->vertices.end(), edge.getSource()) != this->vertices.end())){
+        this->vertices.push_back(edge.getSource());
+        this->currentVerticeCount++;
+    }
+    if(!(find(this->vertices.begin(), this->vertices.end(), edge.getDestination()) != this->vertices.end())){
+        this->vertices.push_back(edge.getDestination());
+        this->currentVerticeCount++;
+    }
+
     this->edges.push_back(edge);
-    this->currentEdgeCount++;
 }
 
 void CityGraph::print(){
@@ -57,6 +76,85 @@ void CityGraph::print(){
 
 void CityGraph::sortEdges(){
     sort(this->edges.begin(), this->edges.end());
+}
+
+int CityGraph::findParent(map<string,int> vertices, int numericVertex){
+    int i=0;
+    int parent;
+
+    for(auto x: vertices){
+        if(i != numericVertex){
+            i++;
+            continue;
+        }
+        else{
+            parent = x.second;
+            break;
+        }
+    }
+
+    if(parent == numericVertex){
+        return parent;
+    }
+    else{
+        return findParent(vertices,parent);
+    }
+}
+
+void CityGraph::createParents(){
+    for(int i=0; i< this->currentVerticeCount; i++){
+        pair<string,int> temp = make_pair(this->vertices[i],i);
+        this->parents.insert(temp);
+    }
+    for(auto x: this->parents){
+        cout<<x.first<<" : "<<x.second<<endl;
+    }
+}
+
+void CityGraph::changeParents(map<string,int> parent, int src, int dest){
+    pair<string, int> temp;
+    int i=0;
+    for(auto node: parent){
+        if(i != src){
+            i++;
+            continue;
+        }
+        else{
+            temp = node;
+            break;
+        }
+    }
+
+    int j=0;
+    for(auto node: parent){
+        if(j != dest){
+            j++;
+            continue;
+        }
+        else{
+            node.second = temp.second;
+        }
+    }
+}
+
+void CityGraph::kruskalAlgorithm(){
+    vector<Edge> temp = this->edges;
+
+    createParents();
+    for(int i=0; i < this->currentVerticeCount; i++){
+        Edge curEdge = temp[i];
+        cout<<endl<<curEdge.getSource();
+        int src = this->parents.at(curEdge.getSource());
+        int dest = this->parents.at(curEdge.getDestination());
+        cout<<src<<" "<<"dest";
+        int srcParent= findParent(this->parents, src);
+        int destParent = findParent(this->parents, dest);
+        cout<<"1111111111"<<endl;
+        if(src != dest){
+            this->minimumSpanningTree.push_back(curEdge);
+            changeParents(this->parents, srcParent, destParent);
+        }
+    }
 }
 
 int main(){
@@ -87,6 +185,8 @@ int main(){
 
     Monstantinapolis.sortEdges(); //sorting edges in ascending order
     Monstantinapolis.print();
-
+    Monstantinapolis.kruskalAlgorithm();
+    Monstantinapolis.print();
+    
     return EXIT_SUCCESS;
 }
